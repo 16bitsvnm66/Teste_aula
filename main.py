f11ae8b936eab04d854722ba5d94236d06b08abc
16bitsvnm66 <f.t.saldanha@gmail.com>
2026-05-28 13:56:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ print(f"Provavelmente nasceste em {ano_nascimento}.")
 print("Obrigado por participares!")
 
 for i in range(3):
+    print(f"Iteração número {i + 1}")
 
 ativo: bool = True
 print(f"Estado ativo: {ativo}")
